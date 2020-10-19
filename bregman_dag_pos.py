840b42340ebfc68a388e548bd780766da8be1f76
manon <manonromain643@hotmail.fr>
2020-10-19 11:28:54 +0200

    small changes to pos

--- bregman_dag_pos.py
+++ bregman_dag_pos.py
@@ -6,8 +6,26 @@ from scipy.linalg import sqrtm
 import mosek.fusion as msk
 
 
-def dyn_no_lips(X, W0, dagness_exp, dagness_pen, l1_pen, verbose=False, logging=False, eps=1e-4, max_iter=500, mosek=True):
-    """ Main algorithm """
+def dyn_no_lips_pos(X, W0, dagness_exp, dagness_pen, l1_pen, eps=1e-4, mosek=True, max_iter=500,
+                    verbose=False, logging=False):
+    """ Main algorithm described in our paper: Ours^+
+
+    Args:
+        X   (np.array): sample matrix
+        W0  (np.array): initial adj matrix
+        dagness_exp (float): alpha in paper
+        dagness_pen (float): mu in paper
+        l1_pen (float): lambda in paper
+        eps    (float): sensivity for early stopping
+        mosek   (bool): solver to use (false is cvxpy)
+        max_iter (int): maximum number of iterations
+        verbose (bool): prints objective value
+        logging (bool): if enabled, returns all objective values + additional info (useful for analysis)
+
+    Returns:
+        Wk     (np.array): current iterate of weighted adj matrix
+        logging_np (dict): if logging is True, dict of metrics/info
+    """
 
     m, n = X.shape
     if m > n:
@@ -39,9 +57,9 @@ def dyn_no_lips(X, W0, dagness_exp, dagness_pen, l1_pen, verbose=False, logging=
         return dagness_pen * (n - 1) * (hWx - hWy - grad_hy_scalar_x_minus_y)
 
     if logging:
-        logging = {"dagness_exp": dagness_exp, "dagness_pen": dagness_pen, "l1_pen": l1_pen,  # constants
-                   "time": [], "ll_trace": [], "l1_val": [], "log_det": [], "dag_constraint": [],
-                   "nb_change_support": [], "support": []}
+        logging_dict = {"dagness_exp": dagness_exp, "dagness_pen": dagness_pen, "l1_pen": l1_pen,  # constants
+                        "time": [], "ll_trace": [], "l1_val": [], "dag_constraint": [],
+                        "nb_change_support": [], "support": []}
 
     # Constants
     lamb = 500
@@ -80,12 +98,12 @@ def dyn_no_lips(X, W0, dagness_exp, dagness_pen, l1_pen, verbose=False, logging=
         # Logging
         if logging:
             support = Wk > 0.5
-            logging["time"].append(time.time() - start)
-            logging["ll_trace"].append(nnl_curr)
-            logging["dag_constraint"].append(dag_penalty_k/dagness_pen)
-            logging["l1_val"].append(np.sum(Wk))
-            logging["nb_change_support"].append(np.sum(prev_support ^ support))
-            logging["support"].append(support.flatten())
+            logging_dict["time"].append(time.time() - start)
+            logging_dict["ll_trace"].append(nnl_curr)
+            logging_dict["dag_constraint"].append(dag_penalty_k/dagness_pen)
+            logging_dict["l1_val"].append(np.sum(Wk))
+            logging_dict["nb_change_support"].append(np.sum(prev_support ^ support))
+            logging_dict["support"].append(support.flatten())
             prev_support = support
         
         if verbose:
@@ -95,14 +113,14 @@ def dyn_no_lips(X, W0, dagness_exp, dagness_pen, l1_pen, verbose=False, logging=
         pbar.update(1)
     print("Done in", time.time() - start, "s and", it_nolips, "iterations")
     if logging:
-        logging_np = {k: np.array(v) for k, v in logging.items()}
+        logging_np = {k: np.array(v) for k, v in logging_dict.items()}
         return Wk, logging_np
     else:
         return Wk
 
 
 def compute_C(n, Wk_value, dagness_pen, dagness_exp, lamb):
-    # Compute C = grad g - 1/lamb grad h
+    # Compute C = grad f - 1/lamb grad h
     Wk_norm = np.linalg.norm(Wk_value, "fro")
     Wk_normalized = Wk_value / Wk_norm
     C = dagness_pen * n * dagness_exp * np.linalg.matrix_power(np.eye(n) + dagness_exp * Wk_value, n - 1)
@@ -112,7 +130,7 @@ def compute_C(n, Wk_value, dagness_pen, dagness_exp, lamb):
 
 def solve_subproblem_mosek(s_mat, Wk_value,
                            lamb, l1_pen, dagness_pen, dagness_exp):
-    """ Solves argmin f(W) + <grad g (Wk), W-Wk>
+    """ Solves argmin g(W) + <grad f (Wk), W-Wk>
                         + 1/lamb * Dh(W, Wk)
 
         this is only implemented for a specific penalty and kernel
@@ -159,7 +177,7 @@ def solve_subproblem_mosek(s_mat, Wk_value,
 
 def solve_subproblem_cvxpy(s_mat, Wk_value,
                            lamb, l1_pen, dagness_pen, dagness_exp):
-    """ Solves argmin f(W) + <grad g (Wk), W-Wk>
+    """ Solves argmin g(W) + <grad f (Wk), W-Wk>
                         + 1/lamb * Dh(W, Wk)
 
         this is only implemented for a specific penalty and kernel
