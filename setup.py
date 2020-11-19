d1d10fab0d2bc8cc38acb1add2ad08dd973c809d
manonromain <manonromain643@hotmail.fr>
2020-11-19 14:36:36 +0100

    renamed notebook and added pip reqs

--- setup.py
+++ setup.py
@@ -19,4 +19,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=["numpy", "scipy", "tqdm", "networkx", "Mosek", "cvxpy", "python-igraph"]
 )
